6a1d64f0c5c0c69af6598887c671903435edd944
suprtrtl <owen.rodway@gmail.com>
2025-05-31 15:19:52 -0600

    gen 103 @ 2025-05-31 15:19 (adaa24f) kernel 6.12.28

--- home-manager-modules/tui/terminal-multiplexers/zellij.nix
+++ home-manager-modules/tui/terminal-multiplexers/zellij.nix
@@ -9,10 +9,7 @@
   };
 
   config = lib.mkIf config.zellij.enable {
-    programs.zellij = {
-      enable = true;
-      enableZshIntegration = true;
-    };
+    programs.zellij.enable = true;
 
     home.file.".config/zellij/config.kdl".source = ./zellij/config.kdl;
   };
