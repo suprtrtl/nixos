3159699c34ef254f3a6dd77e99c887b242114c1f
suprtrtl <owen.rodway@gmail.com>
2025-06-01 13:17:08 -0600

    gen 104 @ 2025-06-01 13:16 (adaa24f) kernel 6.12.28

--- home-manager-modules/tui/terminal-multiplexers/zellij.nix
+++ home-manager-modules/tui/terminal-multiplexers/zellij.nix
@@ -12,5 +12,9 @@
     programs.zellij.enable = true;
 
     home.file.".config/zellij/config.kdl".source = ./zellij/config.kdl;
+    home.file.".config/zellij/plugins/room.wasm".source = pkgs.fetchurl {
+      url = "https://github.com/rvcas/room/releases/latest/download/room.wasm";
+      hash = "sha256-t6GPP7OOztf6XtBgzhLF+edUU294twnu0y5uufXwrkw=";
+    };
   };
 }
