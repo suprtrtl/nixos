7b23b9cc978907da078bd530c31ab69d1b045745
suprtrtl <owen.rodway@gmail.com>
2025-06-22 15:52:46 -0600

    gen 139 @ 2025-06-22 15:52 (910796c) kernel 6.12.31

--- home-manager-modules/gui/notes/obsidian.nix
+++ home-manager-modules/gui/notes/obsidian.nix
@@ -11,10 +11,34 @@
   config = lib.mkIf config.obsidian.enable {
     programs.obsidian = {
       enable = true;
+
+      defaultSettings = {
+        corePlugins = [
+          "backlink"
+          "bookmarks"
+          "canvas"
+          "command-palette"
+          "daily-notes"
+          "editor-status"
+          "file-explorer"
+          "global-search"
+          "graph"
+          "note-composer"
+          "outgoing-link"
+          "outline"
+          "page-preview"
+          "slides"
+          "switcher"
+          "tag-pane"
+          "templates"
+          "word-count"
+        ];
+      };
+
       vaults = {
         "test-vault" = {
           enable = true;
-          target = "vault/test-vault";
+          target = "vaults/test-vault";
         };
       };
     };
