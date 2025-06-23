362a1d72cb5be57b30504c2b925480db736ecf62
suprtrtl <owen.rodway@gmail.com>
2025-06-23 12:28:10 -0600

    gen 141 @ 2025-06-22 17:15 (910796c) kernel 6.12.31

---
+++ home-manager-modules/shells/fish.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "fish-autosuggestions";
+        src = pkgs.fishPlugins.autosuggestions.src;
+      }
+    ];
+    shellAliases = {
+      ll = "ls -l";
+      fman = "complete -C '' | fzf | xargs man";
+    };
+    interactiveShellInit = ''
+      set -g fish_color_autosuggestion 888888
+      fish_add_path $HOME/bin
+    '';
+  };
+  home.file."bin/rebuild-nixos".source = ./../../rebuild-nixos.sh;
+}
