751fcda8792010c9d046514958508f7691cd5f45
suprtrtl <105331009+suprtrtl@users.noreply.github.com>
2025-07-10 12:09:24 -0600

    Create home.nix

---
+++ hosts/wsl/home.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.username = "suprtrtl";  # your WSL username
+  home.homeDirectory = "/home/suprtrtl";
+
+  home.stateVersion = "24.11";
+  nixpkgs.config.allowUnfree = true;
+
+  # CLI tools that work well in WSL
+  bundles.cli.misc.enable = true;
+  neovim.enable = true;
+  btop.enable = true;
+  yazi.enable = true;
+  oh-my-posh.enable = true;
+  tmux.enable = true;
+  zellij.enable = true;
+  fzf.enable = true;
+  zoxide.enable = true;
+
+  # Skip GUI applications
+  # alacritty.enable = true;
+  # ghostty.enable = true;
+  # obsidian.enable = true;
+  # emacs.enable = true;
+
+  home.packages = with pkgs; [
+    # Add any WSL-specific packages
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.home-manager.enable = true;
+}
