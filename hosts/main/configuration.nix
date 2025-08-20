7192bbeb23b7591db2941e48b975ed6ded0987ca
suprtrtl <owen.rodway@gmail.com>
2025-08-20 02:41:06 -0600

    main

---
+++ hosts/main/configuration.nix
@@ -0,0 +1,96 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+  ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Edmonton";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  hardware.bluetooth.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+
+  users.users.suprtrtl = {
+    isNormalUser = true;
+    description = "suprtrtl";
+    extraGroups = ["networkmanager" "wheel"];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      #  thunderbird
+    ];
+  };
+
+  # Home Manager
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    users = {
+      "suprtrtl" = {
+        imports = [
+          ./home.nix
+          inputs.self.outputs.homeManagerModules.default
+        ];
+      };
+    };
+
+    backupFileExtension = "bak";
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Install zsh
+  programs.zsh.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "24.11";
+}
---
+++ hosts/main/home.nix
@@ -0,0 +1,52 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # User Values
+  home.username = "suprtrtl";
+  home.homeDirectory = "/home/suprtrtl";
+
+  home.stateVersion = "24.11";
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Programs
+
+  fzf.enable = true;
+  zoxide.enable = true;
+
+  bundles.cli.misc.enable = true;
+
+  neovim.enable = true;
+  emacs.enable = true;
+  emacs.doom.enable = true;
+
+  obsidian.enable = true;
+
+  alacritty.enable = true;
+  ghostty.enable = true;
+
+  btop.enable = true;
+  rmpc.enable = true;
+
+  yazi.enable = true;
+
+  oh-my-posh.enable = true;
+
+  tmux.enable = true;
+  zellij.enable = true;
+
+  # User specific packages
+  home.packages = with pkgs; [];
+
+  # For dotfiles
+  home.file = {};
+
+  # ENV Variables
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.home-manager.enable = true;
+}
