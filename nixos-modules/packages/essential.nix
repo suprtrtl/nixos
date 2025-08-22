cc642747ba0957f4abb29185420b55db6c113795
suprtrtl <owen.rodway@gmail.com>
2025-08-22 10:56:16 -0600

    gen 55 @ 2025-08-22 10:56 (910796c) kernel 6.12.31

--- nixos-modules/packages/essential.nix
+++ nixos-modules/packages/essential.nix
@@ -12,6 +12,7 @@
     libnotify
     pavucontrol
     neovim
+    networkmanager
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
   ];
