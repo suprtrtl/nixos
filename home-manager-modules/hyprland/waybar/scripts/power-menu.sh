c006d186555554c1bf55753cdcd91317c88b825d
suprtrtl <owen.rodway@gmail.com>
2025-08-25 12:01:15 -0600

    gen 112 @ 2025-08-25 12:01 (910796c) kernel 6.12.31

--- home-manager-modules/hyprland/waybar/scripts/power-menu.sh
+++ home-manager-modules/hyprland/waybar/scripts/power-menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-case "$(printf "kill\nsleep\nreboot\nshutdown" | tofi)" in
+case "$(printf "kill\nsleep\nreboot\nshutdown" | tofi --anchor=top-left --width=5%)" in
 	kill) ps -u $USER -o pid,comm,%cpu,%mem | tofi | awk '{print $1}' | xargs -r kill ;;
 	sleep) systemctl hibernate -i ;;
 	reboot) systemctl reboot -i ;;
