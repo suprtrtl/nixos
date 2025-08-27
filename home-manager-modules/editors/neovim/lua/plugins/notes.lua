cfa9ad9928a01cdf77d8e0ebf8fd4b3dc33f9f70
suprtrtl <owen.rodway@gmail.com>
2025-08-27 13:47:13 -0600

    gen 137 @ 2025-08-27 13:47 (910796c) kernel 6.12.31

--- home-manager-modules/editors/neovim/lua/plugins/notes.lua
+++ home-manager-modules/editors/neovim/lua/plugins/notes.lua
@@ -59,21 +59,6 @@ return {
 		},
 	},
 
-	function ()
-		if os.getenv("KITTY_IMAGE_PROTOCOL") == '1' then
-			return {
-				"3rd/image.nvim",
-				build = false,
-				opts = {
-					processor = "magick_cli",
-				},
-			}
-		else
-			return {}
-		end
-
-	end,
-
 	{
 		"jghauser/follow-md-links.nvim",
 		ft = "markdown",
