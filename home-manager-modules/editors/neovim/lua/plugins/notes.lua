2512cda210fb18c6ba895edb196be3574ecd9c7f
suprtrtl <owen.rodway@gmail.com>
2025-08-27 13:51:21 -0600

    gen 138 @ 2025-08-27 13:51 (910796c) kernel 6.12.31

--- home-manager-modules/editors/neovim/lua/plugins/notes.lua
+++ home-manager-modules/editors/neovim/lua/plugins/notes.lua
@@ -32,6 +32,7 @@ return {
 		"epwalsh/obsidian.nvim",
 		version = "*", -- recommended, use latest release instead of latest commit
 		lazy = true,
+		ft = { "markdown" },
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			-- {
