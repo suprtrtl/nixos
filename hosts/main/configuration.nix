4bdbd6f00c74f10cbcee46e68f55250aef5735c5
suprtrtl <owen.rodway@gmail.com>
2025-09-01 16:21:36 -0600

    gen Unknown @   () kernel

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -11,6 +11,8 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.editor = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = false;
 
