f92841f9829463cfeb3a619dbf4e8f1fc5fbaad7
suprtrtl <owen.rodway@gmail.com>
2025-10-06 16:33:22 -0600

    gen 37 @ 2025-10-06 16:33 (Unknown) kernel 6.12.49

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-case "$(printf "applications\ntools\npower" | tofi  )" in
-	applications) tofi-drun --drun-launch=true ;;
+case "$(printf "  Applications\nTools\nPower" | tofi  )" in
+	  Applications) tofi-drun --drun-launch=true ;;
 	tools) 
 		case "$(printf "screenshot\npicker" | tofi )" in
 			screenshot) notify-send "Screenshot" ;;
 			picker) hyprpicker ;;
+
+			*) exit 1 ;;
+		esac ;;
+	power) 
+		case "$(printf "kill\nsleep\nreboot\nshutdown" | tofi --width=20% --height=20% --num-results=4 --prompt-text="" --placeholder-text="power option:" )" in
+			kill) ps -u $USER -o pid,comm,%cpu,%mem | tofi --width=25% | awk '{print $1}' | xargs -r kill ;;
+			sleep) systemctl suspend -i && hyprlock ;;
+			reboot) systemctl reboot -i ;;
+			shutdown) systemctl poweroff -i ;;
 
 			*) exit 1 ;;
 		esac ;;
