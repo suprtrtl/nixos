658f21a8105cb535aea71347037fcecb59c87612
suprtrtl <owen.rodway@gmail.com>
2025-10-06 16:28:15 -0600

    gen 36 @ 2025-10-06 16:28 (Unknown) kernel 6.12.49

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 case "$(printf "applications\ntools\npower" | tofi  )" in
-	applications) tofi-drun --drun-launch ;;
+	applications) tofi-drun --drun-launch=true ;;
 	tools) 
 		case "$(printf "screenshot\npicker" | tofi )" in
 			screenshot) notify-send "Screenshot" ;;
