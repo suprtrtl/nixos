72a7589ebda871c465eed7338484fd44f1e30184
suprtrtl <owen.rodway@gmail.com>
2025-11-27 16:51:29 -0700

    gen 237 @ 2025-11-27 16:51 (Unknown) kernel 6.12.58

--- nixos-modules/packages/essential.nix
+++ nixos-modules/packages/essential.nix
@@ -23,6 +23,7 @@
     rustc
     neovim
     networkmanager
+    nix-search-cli
     tldr
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     unzip
