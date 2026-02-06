0c22fcd2e21baeeb5b62d939c71ba906b25d07fa
suprtrtl <owen.rodway@gmail.com>
2026-02-06 14:44:25 -0700

    gen 323 @ 2026-02-06 14:44 (Unknown) kernel 6.12.68

--- scripts/zig-version.sh
+++ scripts/zig-version.sh
@@ -1,5 +1,5 @@
 version=$(printf "master\nlatest\n0.15.2\n0.15.1\n0.15.0" | fzf)
-if [ $version -ne "" ]; then
+if [[ "$version" != "" ]]; then
 	nix shell "github:mitchellh/zig-overlay#$version"
 fi
 	
