35cd194b4680aedbf7b57c1a0f8c03a3882bc2d8
suprtrtl <owen.rodway@gmail.com>
2026-02-06 15:07:22 -0700

    gen 327 @ 2026-02-06 15:07 (Unknown) kernel 6.12.68

--- scripts/zig-version.sh
+++ scripts/zig-version.sh
@@ -1,11 +1,11 @@
 version=$(printf "master\nlatest\n0.15.2\n0.15.1\n0.14.1\n0.14.0\n0.13.0" | fzf)
 if [[ "$version" != "" ]]; then
 	echo "Launching Zig Shell ($version)"
+	echo "Make sure you use 'exit' to exit the shell"
 	if [[ "$version" == "latest" ]]; then
 		nix shell "github:mitchellh/zig-overlay"
 	else
 	nix shell "github:mitchellh/zig-overlay#\"$version\""
 	fi
-	echo "Make sure you use 'exit' to exit the shell"
 fi
 	
