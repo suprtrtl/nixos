03cb237d181bf5353f886cab103abbe2d109f54f
suprtrtl <owen.rodway@gmail.com>
2026-02-06 14:54:23 -0700

    gen 324 @ 2026-02-06 14:54 (Unknown) kernel 6.12.68

--- scripts/zig-version.sh
+++ scripts/zig-version.sh
@@ -1,5 +1,9 @@
-version=$(printf "master\nlatest\n0.15.2\n0.15.1\n0.15.0" | fzf)
+version=$(printf "master\nlatest\n0.15.2\n0.15.1\n0.14.1\n0.14.0\n0.13.0" | fzf)
 if [[ "$version" != "" ]]; then
-	nix shell "github:mitchellh/zig-overlay#$version"
+	if [[ "$version" == "latest" ]]; then
+		nix shell "github:mitchellh/zig-overlay"
+	else
+	nix shell "github:mitchellh/zig-overlay#\"$version\""
+	fi
 fi
 	
