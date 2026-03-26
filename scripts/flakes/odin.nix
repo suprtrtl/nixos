4b40870e6f08d6191c957e1059050f318944d3c6
suprtrtl <owen.rodway@gmail.com>
2026-03-26 17:01:08 -0600

    gen 374 @ 2026-03-26 17:01 (Unknown) kernel 6.18.18

---
+++ scripts/flakes/odin.nix
@@ -0,0 +1,35 @@
+{
+  description = "An Odin dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "odin-dev-shell";
+
+          packages = [
+            pkgs.odin
+            pkgs.ols
+          ];
+
+          shellHook = ''
+            echo "Welcome to the Odin development environment for ${system}!"
+          '';
+        };
+      }
+    );
+}
