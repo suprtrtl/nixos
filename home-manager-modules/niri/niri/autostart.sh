601a3a62325a2335c59acca7ffb2f90d81320fb1
suprtrtl <owen.rodway@gmail.com>
2026-04-07 12:12:11 -0600

    gen 389 @ 2026-04-07 12:12 (Unknown) kernel 6.18.20

--- home-manager-modules/niri/niri/autostart.sh
+++ home-manager-modules/niri/niri/autostart.sh
@@ -1,4 +1,4 @@
-waybar -c ~/.config/waybar/config.niri.jsonc -s ~/.config/waybar/style.niri.css
+waybar -c ~/.config/waybar/config.niri.jsonc -s ~/.config/waybar/style.niri.css &
 awww-daemon &
 awww img ~/wallpapers/mountain.png
 
