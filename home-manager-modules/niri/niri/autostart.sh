a7792a61e8f35e0a726532426e45644965dd9f47
suprtrtl <owen.rodway@gmail.com>
2026-06-28 14:15:29 -0600

    gen 432 @ 2026-06-28 14:15 (Unknown) kernel 6.18.36

--- home-manager-modules/niri/niri/autostart.sh
+++ home-manager-modules/niri/niri/autostart.sh
@@ -1,6 +1,6 @@
 waybar -c ~/.config/waybar/config.niri.jsonc -s ~/.config/waybar/style.niri.css &
 awww-daemon &
-awww img ~/wallpapers/od_space02.png
+awww img ~/wallpapers/mountain.png
 swayidle -w \
   timeout 300 'niri msg action power-off-monitors' \
   timeout 360 'swaylock &' \
