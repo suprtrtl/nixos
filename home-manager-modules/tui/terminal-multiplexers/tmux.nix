e417e6bb79d1c6e51793cd3390d8f613c235e2ea
suprtrtl <owen.rodway@gmail.com>
2026-07-03 23:42:54 -0600

    gen 464 @ 2026-07-03 23:42 (Unknown) kernel 6.18.36

--- home-manager-modules/tui/terminal-multiplexers/tmux.nix
+++ home-manager-modules/tui/terminal-multiplexers/tmux.nix
@@ -22,24 +22,39 @@
       shortcut = "a";
 
       extraConfig = ''
-                    set-option -g status-position top
-                    set-option -g allow-passthrough all
+                            set-option -g status-position top
+                            set-option -g allow-passthrough all
 
-              bind h select-pane -L
-              bind j select-pane -D
-              bind k select-pane -U
-              bind l select-pane -R
+                      bind h select-pane -L
+                      bind j select-pane -D
+                      bind k select-pane -U
+                      bind l select-pane -R
 
-        unbind %
-        unbind \"
+                unbind %
+                unbind \"
 
-        bind v split-window -h
-        bind - split-window -v
+                bind v split-window -h
+                bind - split-window -v
 
-                    bind -r H resize-pane -L 5
-                    bind -r J resize-pane -D 5
-                    bind -r K resize-pane -U 5
-                    bind -r L resize-pane -R 5
+                            bind -r H resize-pane -L 5
+                            bind -r J resize-pane -D 5
+                            bind -r K resize-pane -U 5
+                            bind -r L resize-pane -R 5
+
+        bind-key "T" run-shell "sesh connect \"$(
+          sesh list --icons | fzf-tmux -p 80%,70% \
+            --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
+            --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
+            --bind 'tab:down,btab:up' \
+            --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
+            --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+            --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
+            --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+            --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+            --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+            --preview-window 'right:55%' \
+            --preview 'sesh preview {}'
+        )\""
       '';
 
       plugins = with pkgs.tmuxPlugins; [
